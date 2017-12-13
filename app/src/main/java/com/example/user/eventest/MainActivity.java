b38e38411a26739c34c250bc226a2ffaa085abbc
Dima-1 <softmail32@gmail.com>
2017-12-13 12:40:41 +0300

    add test widget

--- app/src/main/java/com/example/user/eventest/MainActivity.java
+++ app/src/main/java/com/example/user/eventest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.user.eventest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        String.valueOf(isChecked), Toast.LENGTH_SHORT);
+                toast.show();
+
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/user/eventest/widget/WidgetProvider.java
@@ -0,0 +1,37 @@
+package com.example.user.eventest.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.util.Log;
+
+import java.util.Arrays;
+
+public class WidgetProvider extends AppWidgetProvider {
+    final String TAG = "TestWidgetLogs";
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d(TAG, "onEnabled");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.d(TAG, "onUpdate " + Arrays.toString(appWidgetIds));
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        Log.d(TAG, "onDeleted " + Arrays.toString(appWidgetIds));
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        Log.d(TAG, "onDisabled");
+    }
+}
