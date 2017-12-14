2f4beac2f3614a92cb767d66035cffe6f07cce62
Dima-1 <softmail32@gmail.com>
2017-12-14 21:16:54 +0300

    widget displays tested preference

--- app/src/main/java/com/example/user/eventest/MainActivity.java
+++ app/src/main/java/com/example/user/eventest/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.user.eventest;
 
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Toast;
+
+import com.example.user.eventest.widget.WidgetProvider;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,9 +26,25 @@ public class MainActivity extends AppCompatActivity {
                 Toast toast = Toast.makeText(getApplicationContext(),
                         String.valueOf(isChecked), Toast.LENGTH_SHORT);
                 toast.show();
-
+                saveState(isChecked);
+                updateWidget();
             }
         });
     }
 
+    private void saveState(boolean isChecked) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("test_state", String.valueOf(isChecked));
+        editor.commit();
+    }
+
+    private void updateWidget() {
+        Intent intent = new Intent(this, WidgetProvider.class);
+        intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        int[] ids = AppWidgetManager.getInstance(getApplication())
+                .getAppWidgetIds(new ComponentName(getApplication(), WidgetProvider.class));
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, ids);
+        sendBroadcast(intent);
+    }
 }
--- app/src/main/java/com/example/user/eventest/widget/WidgetProvider.java
+++ app/src/main/java/com/example/user/eventest/widget/WidgetProvider.java
@@ -3,12 +3,19 @@ package com.example.user.eventest.widget;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.RemoteViews;
+
+import com.example.user.eventest.R;
 
 import java.util.Arrays;
 
 public class WidgetProvider extends AppWidgetProvider {
-    final String TAG = "TestWidgetLogs";
+    static final String TAG = "TestWidgetLogs";
 
     @Override
     public void onEnabled(Context context) {
@@ -20,7 +27,36 @@ public class WidgetProvider extends AppWidgetProvider {
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+
         Log.d(TAG, "onUpdate " + Arrays.toString(appWidgetIds));
+        for (int widgetID : appWidgetIds) {
+            updateWidget(context, appWidgetManager, widgetID);
+        }
+    }
+
+    static void updateWidget(Context context, AppWidgetManager appWidgetManager,
+                             int widgetID) {
+        try {
+            Context mainAppContext;
+
+            mainAppContext = context.createPackageContext("com.example.user.eventest", 0);
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(mainAppContext);
+//            SharedPreferences pref = mainAppContext.getSharedPreferences(
+//                    "test_state", Context.MODE_PRIVATE);
+            String data = pref.getString("test_state", "No Value");
+            Log.d(TAG, "test_state " + data);
+            RemoteViews widgetView = new RemoteViews(context.getPackageName(),
+                    R.layout.widget);
+            widgetView.setTextViewText(R.id.tvCheck, data);
+            Intent updateIntent = new Intent(context, WidgetProvider.class);
+            updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, new int[]{widgetID});
+            appWidgetManager.updateAppWidget(widgetID, widgetView);
+            //   pIntent = PendingIntent.getBroadcast(context, widgetID, updateIntent, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.e("Not data shared", e.toString());
+        }
     }
 
     @Override
