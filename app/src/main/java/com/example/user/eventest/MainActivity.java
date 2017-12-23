da88786af779112e52de1143077ded28b1bea36d
Dima-1 <softmail32@gmail.com>
2017-12-23 23:52:24 +0300

    change date TextView

--- app/src/main/java/com/example/user/eventest/MainActivity.java
+++ app/src/main/java/com/example/user/eventest/MainActivity.java
@@ -7,13 +7,16 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     public final static String PREF_TEST_STATE = "test_state";
@@ -25,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         final ListView lvEvents = findViewById(R.id.lvEvents);
         CheckBox checkBox = findViewById(R.id.checkBox);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        TextView textView = findViewById(R.id.tvDate);
 
         final ArrayList<String> list = new ArrayList<>();
         for (int i = 0; i < 10; ++i) {
@@ -41,15 +45,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context,
-                        String.valueOf(isChecked), Toast.LENGTH_SHORT);
-                toast.show();
+                Toast.makeText(context,
+                        String.valueOf(isChecked), Toast.LENGTH_SHORT).show();
                 new SaveStateAsyncTask(context)
                         .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, isChecked);
                 String TAG = "on click " + this.getClass().getName();
                 Log.d(TAG, "AsyncTask.Status.RUNNING");
                 new UpdateWidgetAsyncTask(context)
                         .executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+            }
+        });
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        String.valueOf(new Date().getTime()), Toast.LENGTH_SHORT).show();
+
             }
         });
     }
