b17bce31e3632f3714e2f6fa26ff2fa4cfa9126c
Dima-1 <softmail32@gmail.com>
2018-01-05 23:57:19 +0300

    date picker pre Lollipop

--- app/src/main/java/com/example/user/eventest/DateTimeDialog.java
+++ app/src/main/java/com/example/user/eventest/DateTimeDialog.java
@@ -7,12 +7,14 @@ package com.example.user.eventest;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 
 import com.example.user.eventest.eventbus.events.DatePickerUpdateEvent;
@@ -23,6 +25,8 @@ import java.util.Calendar;
 import de.greenrobot.event.EventBus;
 
 public class DateTimeDialog extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+    boolean isDatePickerShown = false;
+
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -32,7 +36,24 @@ public class DateTimeDialog extends DialogFragment implements DatePickerDialog.O
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         LayoutInflater li = LayoutInflater.from(getActivity());
-        View promptsView = li.inflate(R.layout.date_time_pick, null);
+        final View promptsView = li.inflate(R.layout.date_time_pick, null);
+        final Button butDateTime = promptsView.findViewById(R.id.butDateTime);
+
+        butDateTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isDatePickerShown) {
+                    promptsView.findViewById(R.id.datePicker).setVisibility(View.INVISIBLE);
+                    promptsView.findViewById(R.id.timePicker).setVisibility(View.VISIBLE);
+                    butDateTime.setText(R.string.date);
+                } else {
+                    promptsView.findViewById(R.id.datePicker).setVisibility(View.VISIBLE);
+                    promptsView.findViewById(R.id.timePicker).setVisibility(View.INVISIBLE);
+                    butDateTime.setText(R.string.time);
+                }
+                isDatePickerShown = !isDatePickerShown;
+            }
+        });
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
         alertDialogBuilder.setView(promptsView);
@@ -56,6 +77,16 @@ public class DateTimeDialog extends DialogFragment implements DatePickerDialog.O
         AlertDialog alertDialog = alertDialogBuilder.create();
         return alertDialog;
 
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            int width = getResources().getDimensionPixelSize(R.dimen.popup_width);
+            int height = getResources().getDimensionPixelSize(R.dimen.popup_height);
+            getDialog().getWindow().setLayout(width, height);
+        }
     }
 
     @Override
