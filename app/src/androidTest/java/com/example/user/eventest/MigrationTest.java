c3a28168fa1397892d7c27606e73b184b92ca87f
Dima-1 <softmail32@gmail.com>
2018-01-16 12:47:28 +0300

    testing database migration

---
+++ app/src/androidTest/java/com/example/user/eventest/MigrationTest.java
@@ -0,0 +1,59 @@
+package com.example.user.eventest;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.db.framework.FrameworkSQLiteOpenHelperFactory;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.testing.MigrationTestHelper;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+import static com.example.user.eventest.AppDatabase.MIGRATION_1_2;
+import static com.example.user.eventest.AppDatabase.MIGRATION_2_3;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by User on 14.01.2018.
+ * testing the migration from version 2 to version 3
+ */
+@RunWith(AndroidJUnit4.class)
+public class MigrationTest {
+    @Rule
+    public MigrationTestHelper testHelper =
+            new MigrationTestHelper(
+                    InstrumentationRegistry.getInstrumentation(),
+                    AppDatabase.class.getCanonicalName(),
+                    new FrameworkSQLiteOpenHelperFactory());
+
+
+    @Test
+    public void testMigration() throws IOException {
+        SupportSQLiteDatabase db =
+                testHelper.createDatabase(AppDatabase.DATABASE_NAME, 2);
+        Memo testMemo = new Memo("21/12/32", "test");
+        db.execSQL("INSERT INTO Memo(name, note) " +
+                "VALUES ('" + testMemo.getDate() + "','" + testMemo.getNote() + "')");
+        db.close();
+
+        testHelper.runMigrationsAndValidate(
+                AppDatabase.DATABASE_NAME, 3, true, MIGRATION_2_3);
+        Memo dbMemo = getMigratedRoomDatabase().getMemoDAO()
+                .getConcreteMemo(testMemo.getDate(), testMemo.getNote());
+        assertEquals(dbMemo.getNote(), testMemo.getNote());
+        assertEquals(dbMemo.getDate(), testMemo.getDate());
+    }
+
+    private AppDatabase getMigratedRoomDatabase() {
+        AppDatabase database = Room.databaseBuilder(InstrumentationRegistry.getTargetContext(),
+                AppDatabase.class, AppDatabase.DATABASE_NAME)
+                .addMigrations(MIGRATION_1_2, MIGRATION_2_3)
+                .build();
+        testHelper.closeWhenFinished(database);
+        return database;
+    }
+}
