1d13770643830ccba1f28f623476d9b71664312f
Dima-1 <softmail32@gmail.com>
2018-03-29 22:43:05 +0300

    MemoAdapter in Kotlin

---
+++ app/src/main/java/com/example/user/eventest/MemoAdapter.kt
@@ -0,0 +1,56 @@
+package com.example.user.eventest
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.user.eventest.model.Memo
+
+/**
+ * Created by User on 11.01.2018.
+ * Adapter for list of memo
+ */
+
+class MemoAdapter(context: Context, private val eventsData: EventsData) :
+        ArrayAdapter<Memo>(context, R.layout.list_row, eventsData.getAllData()) {
+
+    override fun hasStableIds(): Boolean = true
+
+    override fun getItemId(position: Int): Long {
+        val memo: Memo = getItem(position)
+        return memo.memoID
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val tmpView: View
+        val viewHolder: ViewHolder
+        if (convertView == null) {
+            tmpView = LayoutInflater.from(context)
+                    .inflate(R.layout.list_row, parent, false)
+            viewHolder = ViewHolder(tmpView.findViewById(R.id.tvContent),
+                    tmpView.findViewById(R.id.tvDate),
+                    tmpView.findViewById(R.id.tvTime))
+            tmpView.tag = viewHolder
+        } else {
+            tmpView = convertView
+            viewHolder = tmpView.tag as ViewHolder
+        }
+        val memo = getItem(position)
+        if (memo != null) {
+            viewHolder.tvContent.text = memo.note
+            viewHolder.tvDate.text = memo.getDateString()
+            viewHolder.tvTime.text = memo.getTimeString()
+        }
+        return tmpView
+    }
+
+    fun refreshEvents() {
+        clear()
+        addAll(eventsData.getAllData())
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(val tvContent: TextView, val tvDate: TextView, val tvTime: TextView)
+}
